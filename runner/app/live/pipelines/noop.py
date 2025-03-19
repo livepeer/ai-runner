55952555a42ae946437b04c8fe0eec4ee9064818
Varshith B <varshith15@gmail.com>
2025-03-19 17:40:01 +0530

    feat: temp workaround

--- runner/app/live/pipelines/noop.py
+++ runner/app/live/pipelines/noop.py
@@ -2,13 +2,14 @@ from PIL import Image
 import logging
 
 from .interface import Pipeline
+from trickle import VideoFrame, VideoOutput
 
 class Noop(Pipeline):
   def __init__(self, **params):
     super().__init__(**params)
 
-  def process_frame(self, image: Image.Image) -> Image.Image:
-    return image.convert("RGB")
+  def process_frame(self, frame: VideoFrame) -> VideoOutput:
+    return VideoOutput(frame.replace_image(frame.image.convert("RGB")))
 
   def update_params(self, **params):
     logging.info(f"Updating params: {params}")
