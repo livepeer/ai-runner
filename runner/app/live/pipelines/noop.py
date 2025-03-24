22258d68464028b8d015968121de9f40c87cfb78
Varshith B <varshith15@gmail.com>
2025-03-25 00:52:11 +0530

    fix: temp async

--- runner/app/live/pipelines/noop.py
+++ runner/app/live/pipelines/noop.py
@@ -1,5 +1,7 @@
-from PIL import Image
 import logging
+import asyncio
+from PIL import Image
+
 
 from .interface import Pipeline
 from trickle import VideoFrame, VideoOutput
@@ -7,9 +9,18 @@ from trickle import VideoFrame, VideoOutput
 class Noop(Pipeline):
   def __init__(self, **params):
     super().__init__(**params)
+    self.frame_queue = asyncio.Queue()
 
-  def process_frame(self, frame: VideoFrame) -> VideoOutput:
-    return VideoOutput(frame.replace_image(frame.image.convert("RGB")))
+  async def put_video_frame(self, frame: VideoFrame):
+    await self.frame_queue.put(frame)
+
+  async def get_video_frame(self) -> VideoOutput:
+    frame = await self.frame_queue.get()
+    processed_frame = frame.image.convert("RGB")
+    return VideoOutput(frame.replace_image(processed_frame))
 
   def update_params(self, **params):
     logging.info(f"Updating params: {params}")
+
+  async def stop(self):
+    logging.info("Stopping pipeline")
