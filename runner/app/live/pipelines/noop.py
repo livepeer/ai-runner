384915dfdf43972b7fd9d076e46d2b1005fff388
Victor Elias <victorgelias@gmail.com>
2025-04-09 21:09:45 -0300

    trickle: Create similar replace_image helper on VideoOutput

--- runner/app/live/pipelines/noop.py
+++ runner/app/live/pipelines/noop.py
@@ -16,7 +16,7 @@ class Noop(Pipeline):
   async def get_processed_video_frame(self) -> VideoOutput:
     out = await self.frame_queue.get()
     processed_frame = out.image.convert("RGB")
-    return VideoOutput(out.frame.replace_image(processed_frame), out.request_id)
+    return out.replace_image(processed_frame)
 
   async def initialize(self, **params):
     logging.info(f"Initializing Noop pipeline with params: {params}")
