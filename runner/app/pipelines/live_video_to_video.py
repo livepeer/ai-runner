26f1277d4930dab4f30b04060e7cef6cc706cbf7
John | Elite Encoder <john@eliteencoder.net>
2025-04-10 05:31:59 +0000

    fix: use conda env only when pipeline is comfyui

--- runner/app/pipelines/live_video_to_video.py
+++ runner/app/pipelines/live_video_to_video.py
@@ -107,7 +107,10 @@ class LiveVideoToVideoPipeline(Pipeline):
             raise ConnectionError(f"Failed to get status: {e}")
 
     def start_process(self, **kwargs):
-        cmd = [str("/workspace/miniconda3/envs/comfystream/bin/python"), str(self.infer_script_path)]
+        if kwargs.get("pipeline") == "comfyui":
+            cmd = ["conda", "run", "--no-capture-output", "-n", "comfystream", "python", str(self.infer_script_path)]
+        else:
+            cmd = [str(self.infer_script_path)]
 
         # Add any additional kwargs as command-line arguments
         for key, value in kwargs.items():
