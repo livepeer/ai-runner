c65ed9bc615ddd9f9b92b25eb3df0bed4104db86
John | Elite Encoder <john@eliteencoder.net>
2025-05-14 22:40:07 +0000

    fix noop

--- runner/app/live/pipelines/noop.py
+++ runner/app/live/pipelines/noop.py
@@ -13,10 +13,10 @@ class Noop(Pipeline):
   async def put_video_frame(self, frame: VideoFrame, request_id: str):
     await self.frame_queue.put(VideoOutput(frame, request_id))
 
-  async def get_processed_video_frame(self, request_id: str) -> VideoOutput:
+  async def get_processed_video_frame(self) -> VideoOutput:
     out = await self.frame_queue.get()
     processed_frame = out.image.convert("RGB")
-    return VideoOutput(out.frame.replace_image(processed_frame), request_id)
+    return VideoOutput(out.frame.replace_image(processed_frame), out.request_id)
 
   async def initialize(self, **params):
     logging.info(f"Initializing Noop pipeline with params: {params}")
