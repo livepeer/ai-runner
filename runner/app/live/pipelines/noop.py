5060d0a3ca539369ad647966f72ca4c7eda00fcc
Victor Elias <victorgelias@gmail.com>
2025-05-17 13:16:18 -0300

    Revert "pipelines: Make noop hang after 10s for testing"
    This reverts commit 6cf6cc4ee0a50cdecebeb46885c38d7dbdc40649.

--- runner/app/live/pipelines/noop.py
+++ runner/app/live/pipelines/noop.py
@@ -1,7 +1,7 @@
 import logging
 import asyncio
-import time
-import os
+from PIL import Image
+
 
 from .interface import Pipeline
 from trickle import VideoFrame, VideoOutput
@@ -9,26 +9,9 @@ from trickle import VideoFrame, VideoOutput
 class Noop(Pipeline):
   def __init__(self):
     self.frame_queue: asyncio.Queue[VideoOutput] = asyncio.Queue()
-    self.timeout_started = False
-    self.start_time = None
 
   async def put_video_frame(self, frame: VideoFrame, request_id: str):
-    if not self.start_time:
-      self.start_time = time.time()
-    # stop producing frames after 10 seconds
-    if time.time() - self.start_time < 10:
-      await self.frame_queue.put(VideoOutput(frame, request_id))
-    elif not hasattr(self, "skipping_frames"):
-      self.skipping_frames = True
-      logging.info("Skipping frames from now on")
-
-    # kill the process after 60 seconds
-    # if not self.timeout_started:
-    #   self.timeout_started = True
-    #   async def kill_after_timeout():
-    #     await asyncio.sleep(60)
-    #     os._exit(1)
-    #   asyncio.create_task(kill_after_timeout())
+    await self.frame_queue.put(VideoOutput(frame, request_id))
 
   async def get_processed_video_frame(self) -> VideoOutput:
     out = await self.frame_queue.get()
