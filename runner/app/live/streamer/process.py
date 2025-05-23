3e23a80ca9f4b402603ca80f248237515fedac3c
Rafał Leszko <rafal@livepeer.org>
2025-05-23 10:10:27 +0200

    Revert "live/streamer: Improve monitor loop reliability (#568)"
    This reverts commit a0c77a6b1e45ec8485b29236dde8e02f5c01fd13.

--- runner/app/live/streamer/process.py
+++ runner/app/live/streamer/process.py
@@ -33,7 +33,6 @@ class PipelineProcess:
         self.error_queue = self.ctx.Queue()
         self.log_queue = self.ctx.Queue(maxsize=100)  # Keep last 100 log lines
 
-        self.pipeline_initialized = self.ctx.Event()
         self.done = self.ctx.Event()
         self.process = self.ctx.Process(target=self.process_loop, args=())
         self.start_time = 0.0
@@ -72,9 +71,6 @@ class PipelineProcess:
 
     def is_done(self):
         return self.done.is_set()
-
-    def is_pipeline_initialized(self):
-        return self.pipeline_initialized.is_set()
 
     def update_params(self, params: dict):
         self.param_update_queue.put(params)
@@ -165,23 +161,18 @@ class PipelineProcess:
                 return pipeline
         except Exception as e:
             self._report_error(f"Error loading pipeline: {e}")
-            if not params:
-                # Already tried loading with default params
-                raise
-            try:
-                with log_timing(
-                    f"PipelineProcess: Pipeline loading with default params due to error with params: {params}"
-                ):
-                    pipeline = load_pipeline(self.pipeline_name)
-                    await pipeline.initialize()
-                    return pipeline
-            except Exception as e:
-                self._report_error(f"Error loading pipeline with default params: {e}")
-                raise
+            if params:
+                try:
+                    with log_timing(f"PipelineProcess: Pipeline loading with default params due to error with params: {params}"):
+                        pipeline = load_pipeline(self.pipeline_name)
+                        await pipeline.initialize()
+                        return pipeline
+                except Exception as e:
+                    self._report_error(f"Error loading pipeline with default params: {e}")
+                    raise
 
     async def _run_pipeline_loops(self):
         pipeline = await self._initialize_pipeline()
-        self.pipeline_initialized.set()
         input_task = asyncio.create_task(self._input_loop(pipeline))
         output_task = asyncio.create_task(self._output_loop(pipeline))
         param_task = asyncio.create_task(self._param_update_loop(pipeline))
