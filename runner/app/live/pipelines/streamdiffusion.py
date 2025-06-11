b7253a61a6955ae568bdf3b5b7fa99df5401ae94
Victor Elias <victor@livepeer.org>
2025-06-11 12:17:31 +0000

    Create helper functions to run in threads

--- runner/app/live/pipelines/streamdiffusion.py
+++ runner/app/live/pipelines/streamdiffusion.py
@@ -1,9 +1,9 @@
 import logging
 import asyncio
-from typing import Dict, List, Literal, Optional, cast
+from typing import Dict, List, Literal, Optional
 
-from PIL import Image
-from pydantic import BaseModel, Field
+import torch
+from pydantic import BaseModel
 from StreamDiffusionWrapper import StreamDiffusionWrapper
 
 from .interface import Pipeline
@@ -44,29 +44,31 @@ class StreamDiffusion(Pipeline):
         logging.info("Pipeline initialization complete")
 
     async def put_video_frame(self, frame: VideoFrame, request_id: str):
+        out_tensor = await asyncio.to_thread(self.process_tensor_sync, frame.tensor)
+        output = VideoOutput(frame, request_id).replace_tensor(out_tensor)
+        await self.frame_queue.put(output)
+
+    def process_tensor_sync(self, img_tensor: torch.Tensor):
         if self.pipe is None:
             raise RuntimeError("Pipeline not initialized")
 
         # The incoming frame.tensor is (B, H, W, C) in range [-1, 1] while the
         # VaeImageProcessor inside the wrapper expects (B, C, H, W) in [0, 1].
-        img_tensor = frame.tensor.permute(0, 3, 1, 2)
+        img_tensor = img_tensor.permute(0, 3, 1, 2)
         img_tensor = self.pipe.stream.image_processor.denormalize(img_tensor)
         img_tensor = self.pipe.preprocess_image(img_tensor)
 
         if self.first_frame:
             self.first_frame = False
             for _ in range(self.pipe.batch_size):
-                _ = await asyncio.to_thread(self.pipe, image=img_tensor)
+                self.pipe(image=img_tensor)
 
-        out_tensor = await asyncio.to_thread(self.pipe, image=img_tensor)
+        out_tensor = self.pipe(image=img_tensor)
         if isinstance(out_tensor, list):
             out_tensor = out_tensor[0]
 
         # The output tensor from the wrapper is (C, H, W), and the encoder expects (1, H, W, C).
-        out_tensor = out_tensor.permute(1, 2, 0).unsqueeze(0)
-
-        output = VideoOutput(frame, request_id).replace_tensor(out_tensor)
-        await self.frame_queue.put(output)
+        return out_tensor.permute(1, 2, 0).unsqueeze(0)
 
     async def get_processed_video_frame(self) -> VideoOutput:
         return await self.frame_queue.get()
@@ -83,37 +85,39 @@ class StreamDiffusion(Pipeline):
                 return
 
         logging.info(f"Resetting diffuser for params change")
-        def load_streamdiffusion():
-            pipe = StreamDiffusionWrapper(
-                output_type="pt",
-                model_id_or_path=new_params.model_id,
-                lora_dict=new_params.lora_dict,
-                use_lcm_lora=new_params.use_lcm_lora,
-                lcm_lora_id=new_params.lcm_lora_id,
-                t_index_list=new_params.t_index_list,
-                frame_buffer_size=1,
-                width=512,
-                height=512,
-                warmup=10,
-                acceleration=new_params.acceleration,
-                do_add_noise=new_params.do_add_noise,
-                mode="img2img",
-                enable_similar_image_filter=new_params.enable_similar_image_filter,
-                similar_image_filter_threshold=new_params.similar_image_filter_threshold,
-                use_denoising_batch=new_params.use_denoising_batch,
-                seed=new_params.seed,
-            )
-            pipe.prepare(
-                prompt=new_params.prompt,
-                num_inference_steps=new_params.num_inference_steps,
-                guidance_scale=new_params.guidance_scale,
-            )
-            return pipe
 
-        self.pipe = await asyncio.to_thread(load_streamdiffusion)
+        self.pipe = await asyncio.to_thread(load_streamdiffusion_sync, new_params)
         self.params = new_params
         self.first_frame = True
 
     async def stop(self):
         logging.info("Stopping StreamDiffusion pipeline")
         self.pipe = None
+
+
+def load_streamdiffusion_sync(params: StreamDiffusionParams):
+    pipe = StreamDiffusionWrapper(
+        output_type="pt",
+        model_id_or_path=params.model_id,
+        lora_dict=params.lora_dict,
+        use_lcm_lora=params.use_lcm_lora,
+        lcm_lora_id=params.lcm_lora_id,
+        t_index_list=params.t_index_list,
+        frame_buffer_size=1,
+        width=512,
+        height=512,
+        warmup=10,
+        acceleration=params.acceleration,
+        do_add_noise=params.do_add_noise,
+        mode="img2img",
+        enable_similar_image_filter=params.enable_similar_image_filter,
+        similar_image_filter_threshold=params.similar_image_filter_threshold,
+        use_denoising_batch=params.use_denoising_batch,
+        seed=params.seed,
+    )
+    pipe.prepare(
+        prompt=params.prompt,
+        num_inference_steps=params.num_inference_steps,
+        guidance_scale=params.guidance_scale,
+    )
+    return pipe
