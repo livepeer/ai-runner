4eb60b0dc59d0810afb0af3af3fecaefc5ddc41f
Victor Elias <victor@livepeer.org>
2025-11-07 04:46:30 +0000

    tensorrt: Call conda python for raft engine

--- runner/app/tools/streamdiffusion/build_tensorrt_internal.sh
+++ runner/app/tools/streamdiffusion/build_tensorrt_internal.sh
@@ -350,7 +350,7 @@ function build_raft_engine() {
         echo "RAFT TensorRT engine already exists at $engines_dir/raft.engine. Skipping build."
         return 0
     fi
-    python -m streamdiffusion.tools.compile_raft_tensorrt --min_resolution 384 --max_resolution 1024 --output_dir $engines_dir
+    $CONDA_PYTHON -m streamdiffusion.tools.compile_raft_tensorrt --min_resolution 384 --max_resolution 1024 --output_dir $engines_dir
 }
 
 if [ "$BUILD_DEPTH_ANYTHING" = true ]; then
